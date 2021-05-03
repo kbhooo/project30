3137f4f8c2fa71953083e864c7399b26f953aeda
kbhooo <ironman035614@GMAIL.COM>
2021-05-03 22:11:32 +0900

    Modify: Hooks Example

--- hooks-example/src/App.js
+++ hooks-example/src/App.js
@@ -1,9 +1,30 @@
-function App() {
+import React, { Component, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [email, setEmail] = useState("");
+  const updateEmail = e => {
+    const {
+      target: {value}
+    } = e;
+    setEmail(value);
+  };
   return (
-    <div className="App">
-      Hooks
-    </div>
+    <>
+      {count}
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+      <input 
+        placeholder="Email" 
+        value={email} 
+        onChange={updateEmail}
+      />
+    </>
   );
-}
+};
 
 export default App;
